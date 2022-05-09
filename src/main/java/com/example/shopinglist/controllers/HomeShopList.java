f48a267a0837f904d924782d330509c635f23846
SHCHERBA92 <vony4ka666@rambler.ru>
2022-05-09 13:59:54 +0300

    Добавлена главная страница

--- src/main/java/com/example/shopinglist/controllers/HomeShopList.java
+++ src/main/java/com/example/shopinglist/controllers/HomeShopList.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Home {
+public class HomeShopList {
 
     @GetMapping
     public String getHomePage(){
-        return "index";
+        return "home-shop-list";
     }
 
 
