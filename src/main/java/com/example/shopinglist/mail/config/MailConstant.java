5f3992d94a94d18d8c7bb3667939c8c898136511
SHCHERBA92 <vony4ka666@rambler.ru>
2022-07-25 22:46:45 +0300

    Список шоплистов привязан к активному пользователю.

--- src/main/java/com/example/shopinglist/mail/config/MailConstant.java
+++ src/main/java/com/example/shopinglist/mail/config/MailConstant.java
@@ -2,7 +2,7 @@ package com.example.shopinglist.mail.config;
 
 public class MailConstant {
     public static final String USER_NAME = "kirillshcherba92@yandex.ru";
-    static final String PASSWORD = "zxlzinysirgusrqy";  // Обязательно настраивается и на стороне почты получаем
+    static final String PASSWORD = "password";  // Обязательно настраивается и на стороне почты получаем
     static final String PROTOCOL = "smtp";
     static final Integer PORT = 465;
     static final String HOST_NAME = "smtp.yandex.ru";
